741b167532887b46139b86712a3f2495b10560dc
ident1t <v.grund@email.cz>
2024-04-07 11:11:52 +0200

    uprava

--- Programy/app.py
+++ Programy/app.py
@@ -1,10 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def helloworld():
-    return "Hello world!"
+    return render_template("Homepage/index.html")
 
 if __name__ == "_main":
     app.run(host='0.0.0.0', debug=True)
